4731ae465303d2c54767cb437dd646cad3f3179b
lubwizard <matej.luburic@stack-wizard.com>
2022-12-01 11:02:50 +0100

    Add category controller tests.

---
+++ src/test/java/guru/springframework/spring5webfluxrest/controllers/CategoryControllerTest.java
@@ -0,0 +1,60 @@
+package guru.springframework.spring5webfluxrest.controllers;
+
+import guru.springframework.spring5webfluxrest.domain.Category;
+import guru.springframework.spring5webfluxrest.repository.CategoryRepository;
+import org.junit.Before;
+import org.junit.Test;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+import org.springframework.test.web.reactive.server.WebTestClient;
+import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
+
+import static org.junit.Assert.*;
+import static org.mockito.ArgumentMatchers.anyString;
+import static org.mockito.BDDMockito.given;
+
+public class CategoryControllerTest {
+
+    WebTestClient webTestClient;
+
+    @Mock
+    CategoryRepository categoryRepository;
+
+    @InjectMocks
+    CategoryController categoryController;
+
+    @Before
+    public void setUp() throws Exception {
+        MockitoAnnotations.initMocks(this);
+
+        webTestClient = WebTestClient.bindToController(categoryController).build();
+    }
+
+    @Test
+    public void getAllCategories() {
+        //given
+        given(categoryRepository.findAll()).willReturn(
+                Flux.just(
+                        Category.builder().description("Cat1").build(),
+                        Category.builder().description("Cat2").build()
+                ));
+
+        webTestClient.get().uri("/api/v1/categories")
+                .exchange()
+                .expectBodyList(Category.class)
+                .hasSize(2);
+    }
+
+    @Test
+    public void getCategoryById() {
+        given(categoryRepository.findById("someid")).willReturn(
+               Mono.just(Category.builder().description("Cat1").build())
+        );
+
+        webTestClient.get().uri("/api/v1/categories/someid")
+                .exchange()
+                .expectBody(Category.class);
+    }
+}
